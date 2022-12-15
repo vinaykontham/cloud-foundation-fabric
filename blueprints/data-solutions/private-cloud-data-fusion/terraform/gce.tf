dadfc2634c22de44402308da15b22e0fed2df2cf
sylvio <sylvio.pedroza@gmail.com>
2022-12-15 18:54:50 +0000

    migrate gce to modules

--- blueprints/data-solutions/private-cloud-data-fusion/terraform/gce.tf
+++ blueprints/data-solutions/private-cloud-data-fusion/terraform/gce.tf
@@ -18,75 +18,63 @@ data "google_compute_zones" "available" {
   status  = "UP"
 }
 
-resource "google_compute_instance" "cloudsql_proxy" {
-  name         = "cloudsql-proxy-${local.sql_instance_name}"
-  machine_type = "e2-small"
-  zone         = data.google_compute_zones.available.names[0]
-
-  tags = ["allow-ssh"]
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-101-lts"
-      labels = {
-        vm_name = "cloudsql-proxy"
-      }
-    }
+module "vm_cloudsql_proxy" {
+  source                 = "../../../../modules/compute-vm"
+  project_id             = var.project_id
+  zone                   = data.google_compute_zones.available.names[0]
+  name                   = "cloudsql-proxy-${local.sql_instance_name}"
+  instance_type          = "e2-micro"
+  labels                 = var.resource_labels
+  tags                   = ["allow-ssh"]
+  service_account        = module.sql_client_service_account.email
+  service_account_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  boot_disk = {
+    image = "cos-cloud/cos-101-lts"
   }
-
-  network_interface {
+  network_interfaces = [{
     network    = module.vpc.name
     subnetwork = module.vpc.subnet_self_links["${var.region}/gce"]
-  }
-
-  metadata_startup_script = <<EOF
-  curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-  sudo bash add-google-cloud-ops-agent-repo.sh --also-install
-
-  docker run -d -p 0.0.0.0:3306:3306 gcr.io/cloudsql-docker/gce-proxy:latest /cloud_sql_proxy -instances=${google_sql_database_instance.instance.connection_name}=tcp:0.0.0.0:3306
-  EOF
-
-  service_account {
-    email  = module.sql_client_service_account.email
-    scopes = ["cloud-platform"]
+  }]
+  metadata = {
+    startup-script = <<EOF
+    curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+    sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+    docker run -d -p 0.0.0.0:3306:3306 gcr.io/cloudsql-docker/gce-proxy:latest /cloud_sql_proxy -instances=${google_sql_database_instance.instance.connection_name}=tcp:0.0.0.0:3306
+    EOF
   }
 }
 
-resource "google_compute_instance" "mysql_client" {
-  name         = "mysql-client"
-  machine_type = "e2-small"
-  zone         = data.google_compute_zones.available.names[0]
-
-  tags = ["allow-ssh"]
-
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1804-lts"
-    }
+module "vm_mysql_client" {
+  source                 = "../../../../modules/compute-vm"
+  project_id             = var.project_id
+  zone                   = data.google_compute_zones.available.names[0]
+  name                   = "mysql-client"
+  instance_type          = "e2-micro"
+  labels                 = var.resource_labels
+  tags                   = ["allow-ssh"]
+  service_account        = module.sql_client_service_account.email
+  service_account_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  boot_disk = {
+    image = "ubuntu-os-cloud/ubuntu-1804-lts"
   }
-
-  network_interface {
+  network_interfaces = [{
     network    = module.vpc.name
     subnetwork = module.vpc.subnet_self_links["${var.region}/gce"]
-  }
+  }]
+  metadata = {
+    startup-script = <<EOF
+    curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+    sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
-  metadata_startup_script = <<EOF
-  curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-  sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt install mysql-client -y
 
-  sudo apt update
-  sudo apt upgrade -y
-  sudo apt install mysql-client -y
+    git clone https://github.com/datacharmer/test_db.git
+    cd test_db
 
-  git clone https://github.com/datacharmer/test_db.git
-  cd test_db
-
-  MYSQL_IP=${google_compute_instance.cloudsql_proxy.network_interface.0.network_ip}
-  mysql -h$MYSQL_IP -udatafusion -p${var.db_password} < employees.sql
-  EOF
-
-  service_account {
-    email  = module.sql_client_service_account.email
-    scopes = ["cloud-platform"]
+    MYSQL_IP=${google_sql_database_instance.instance.ip_address.0.ip_address}
+    mysql -h$MYSQL_IP -udatafusion -p${var.db_password} < employees.sql
+    EOF
   }
 }
